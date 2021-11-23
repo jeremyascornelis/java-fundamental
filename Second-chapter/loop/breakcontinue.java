8870deb633b1278f826d9e47b71e45a57e2d0c51
Jeremyas Cornelis <74807111+jeremyascornelis@users.noreply.github.com>
2021-11-23 08:30:07 +0700

    Add files via upload

---
+++ Second-chapter/loop/breakcontinue.java
@@ -0,0 +1,25 @@
+public class breakcontinue {
+    public static void main(String[] args) {
+        System.out.println("=== loop while ===");
+        int i = 1;
+        while(i < 10) {
+            //keluar dari loop jika i dapat dibagi 5
+            if(i % 5 == 0) {
+                break;
+            }
+
+            System.out.println(i);
+            i++;
+        }
+
+        System.out.println("=== loop for ===");
+        for (int j = 1; j < 10; j++) {
+            //lewati loop jika j dapat dibagi 3
+            if(j % 3 == 0) {
+                continue;
+            }
+
+            System.out.println(j);
+        }
+    }
+}
