a9d8a9895aa1d5ef2b21befddce4dfe76cb92045
Jeremyas Cornelis <74807111+jeremyascornelis@users.noreply.github.com>
2021-11-23 08:28:43 +0700

    Add files via upload

---
+++ Second-chapter/controlFlow/ifstatement.java
@@ -0,0 +1,18 @@
+class ifstatement {
+    public static void main(String[] args) {
+        if(true) {
+            System.out.println("Kondisi adalah true. Cetak...");
+        }
+
+        int x = 5;
+        //Jika x lebih besar dari 2, cetak "x lebih besar dari 2"
+        if (x > 2) {
+            System.out.println("x lebih besar dari 2");
+        }
+
+        //Jika x lebih besar dari 8, cetak "x lebih besar dari 8"
+        if(x > 8) {
+            System.out.println("x lebih besar dari 8");
+        }
+    }
+}
