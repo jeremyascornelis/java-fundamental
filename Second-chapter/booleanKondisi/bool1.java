af2812af1c6663e2dbf404d2f91ed3c8170c4d6b
Jeremyas Cornelis <74807111+jeremyascornelis@users.noreply.github.com>
2021-11-23 08:29:33 +0700

    Add files via upload

---
+++ Second-chapter/booleanKondisi/bool1.java
@@ -0,0 +1,21 @@
+class bool1 {
+    public static void main(String[] args) {
+        //Cetak true
+        System.out.println(true);
+
+        //Cetak false
+        System.out.println(false);
+
+        //Bandingkan nilai menggunakan == dan cetak hasilnya
+        System.out.println(12 / 4 == 3);
+
+        //Bandingkan nilai menggunakan != dan cetak hasilnya
+        System.out.println(12 / 4 != 3);
+
+        //Deklarasikan variable bool bertipe boolean dan tetapkan hasukbya 3 * 9 = 27 kepadanya
+        boolean bool = 3 * 9 == 27;
+
+        //Cetak
+        System.out.println(bool);
+    }
+}
---
+++ Second-chapter/booleanKondisi/bool2.java
@@ -0,0 +1,9 @@
+public class bool2 {
+    public static void main(String[] args) {
+        //Bandingkan
+        System.out.println(8 < 5);
+
+        //Bandingkan 
+        System.out.println(3 >= 2);
+    }
+}
